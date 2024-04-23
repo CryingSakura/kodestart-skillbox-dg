4c6a469cd16b507dacfe319f126ca41d74e2722b
Danya Goga <CryingSakura69@gmail.com>
2024-04-23 14:50:56 +0200

    add enum classes, create separate fun fetchData, delete data class Profile

--- products/ui/src/main/java/ru/kode/base/internship/ui/home/ProductsHomeMocks.kt
+++ products/ui/src/main/java/ru/kode/base/internship/ui/home/ProductsHomeMocks.kt
@@ -1,83 +1,127 @@
 package ru.kode.base.internship.ui.home
 
+import androidx.compose.runtime.Immutable
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import ru.kode.base.core.util.randomUuid
 import ru.kode.base.internship.core.domain.entity.LceState
+import ru.kode.base.internship.ui.cardicon.PaymentSystem
 import kotlin.random.Random
 import kotlin.time.Duration.Companion.seconds
 
-
 val accountId1: String = randomUuid()
 val accountId2: String = randomUuid()
+val depositId1: String = randomUuid()
+val depositId2: String = randomUuid()
+val depositId3: String = randomUuid()
+val cardId1: String = randomUuid()
+val cardId2: String = randomUuid()
+val cardId3: String = randomUuid()
 
-data class Profile(
-  val id: String,
-  val firstName: String,
-  val middleName: String?,
-  val lastName: String,
-  val country: String,
-  val phone: String,
-  val deposits: List<Deposit>,
-  val accounts: List<Account>,
-)
+enum class CardType {
+  physical,
+  virtual
+}
+enum class CardStatus {
+  Active,
+  Blocked,
+}
+enum class AccountStatus {
+  Active,
+  Blocked,
+}
+enum class DepositStatus {
+  Active,
+  Blocked,
+}
 
+enum class Currency {
+  RUB,
+  USD,
+  EUR
+}
+
+data class Money(
+  val balance: String,
+  val currency: Currency
+) {
+  fun format(): String {
+    return "$balance " + when (currency) {
+      Currency.RUB -> "₽"
+      Currency.USD -> "$"
+      Currency.EUR -> "€"
+    }
+  }
+}
+
+@Immutable
 data class Account(
   val cards: List<Card>,
   val number: String,
-  val status: String,
+  val status: AccountStatus,
   val balance: String,
-  val currency: String,
-  val accountId: String,
-)
-
+  val currency: Currency,
+  val accountId: Id,
+) {
+  @JvmInline
+  value class Id(val accountId: String)
+}
+@Immutable
 data class Deposit(
-  val depositId: String = randomUuid(),
+  val depositId: Id,
   val balance: String,
-  val status: String,
+  val status: DepositStatus,
   val rate: String,
   val closeDate: String,
-  val currency: String,
+  val currency: Currency,
   val name: String,
-)
-
+) {
+  @JvmInline
+  value class Id(val depositId: String)
+}
+@Immutable
 data class Card(
-  val id: String = randomUuid(),
+  val cardId: Id,
   val accountId: String,
   val number: String,
   val expiredAt: String,
-  val status: String,
+  val status: CardStatus,
   val name: String,
-  val cardType: String,
-  val paymentSystem: String,
-)
-
+  val cardType: CardType,
+  val paymentSystem: PaymentSystem,
+) {
+  @JvmInline
+  value class Id(val cardId: String)
+}
 
 val deposit1 = Deposit(
   balance = "5000.00",
-  status = "Active",
+  status = DepositStatus.valueOf("Active"),
   rate = "11.5",
   closeDate = "01.01.2023",
-  currency = "RUB",
-  name = "Savings Deposit"
+  currency = Currency.valueOf("RUB"),
+  name = "Savings Deposit",
+  depositId = Deposit.Id(depositId1)
 )
 
 val deposit2 = Deposit(
   balance = "10000.00",
-  status = "Active",
+  status = DepositStatus.valueOf("Active"),
   rate = "13.0",
   closeDate = "06.30.2022",
-  currency = "USD",
-  name = "Fixed Deposit"
+  currency = Currency.valueOf("USD"),
+  name = "Fixed Deposit",
+  depositId = Deposit.Id(depositId2)
 )
 
 val deposit3 = Deposit(
   balance = "2000.00",
-  status = "Active",
+  status = DepositStatus.valueOf("Active"),
   rate = "7.5",
   closeDate = "12.31.2021",
-  currency = "EUR",
-  name = "Term Deposit"
+  currency = Currency.valueOf("EUR"),
+  name = "Term Deposit",
+  depositId = Deposit.Id(depositId3)
 )
 
 val depositList = listOf(deposit1, deposit2, deposit3)
@@ -86,61 +130,52 @@ val card1 = Card(
   accountId = accountId1,
   number = "1111222233334444",
   expiredAt = "2022-04-21T00:00:00Z",
-  status = "Active",
+  status = CardStatus.valueOf("Active"),
   name = "test1",
-  cardType = "physical",
-  paymentSystem = "Visa"
+  cardType = CardType.valueOf("physical"),
+  paymentSystem = PaymentSystem.valueOf("Visa"),
+  cardId = Card.Id(cardId1)
 )
 
 val card2 = Card(
   accountId = accountId1,
   number = "5555666677778888",
   expiredAt = "2022-04-21T00:00:00Z",
-  status = "Blocked",
+  status = CardStatus.valueOf("Blocked"),
   name = "test2",
-  cardType = "virtual",
-  paymentSystem = "Mastercard"
+  cardType = CardType.valueOf("virtual"),
+  paymentSystem = PaymentSystem.valueOf("Mastercard"),
+  cardId = Card.Id(cardId2)
 )
 val card3 = Card(
   accountId = accountId1,
   number = "5555666677778888",
   expiredAt = "2022-04-21T00:00:00Z",
-  status = "Active",
-  name = "test2",
-  cardType = "virtual",
-  paymentSystem = "Mastercard"
+  status = CardStatus.valueOf("Active"),
+  name = "test3",
+  cardType = CardType.valueOf("virtual"),
+  paymentSystem = PaymentSystem.valueOf("Mastercard"),
+  cardId = Card.Id(cardId3)
 )
 
 val account1 = Account(
-  accountId = accountId1,
+  accountId = Account.Id(accountId1),
   cards = listOf(card1, card2),
   number = "1234567890",
-  status = "Active",
+  status = AccountStatus.valueOf("Active"),
   balance = "1000.00",
-  currency = "RUB"
+  currency = Currency.valueOf("EUR")
 )
 val account2 = Account(
-  accountId = accountId2,
+  accountId = Account.Id(accountId2),
   cards = listOf(card3),
   number = "1234567890",
-  status = "Active",
+  status = AccountStatus.valueOf("Active"),
   balance = "1000.00",
-  currency = "USD"
+  currency = Currency.valueOf("USD")
 )
 
 val accountList = listOf(account1, account2)
-
-val profile = Profile(
-  id = "0",
-  firstName = "Ivan",
-  middleName = "Ivanovich",
-  lastName = "Ivanov",
-  country = "Russia",
-  phone = "+79123213233",
-  deposits = emptyList(),
-  accounts = emptyList()
-)
-
 
 val accountsData = MutableStateFlow<List<Account>>(emptyList())
 val depositsData = MutableStateFlow<List<Deposit>>(emptyList())
@@ -148,28 +183,22 @@ val depositsData = MutableStateFlow<List<Deposit>>(emptyList())
 val dataAccountsState = MutableStateFlow<LceState>(LceState.None)
 val dataDepositsState = MutableStateFlow<LceState>(LceState.None)
 
-suspend fun fetchMoreData() {
-  dataState.value = LceState.Loading
+suspend fun fetchAccountsData() {
   dataAccountsState.value = LceState.Loading
-  dataDepositsState.value = LceState.Loading
   delay(2.seconds)
   if (Random.nextBoolean()) {
     dataAccountsState.value = LceState.Content
     accountsData.value = accountList
   } else {
-    dataState.value = LceState.Error("Failed to load data")
+    dataAccountsState.value = LceState.Error("Failed to load data")
   }
+} suspend fun fetchDepositsData() {
+  dataDepositsState.value = LceState.Loading
+  delay(2.seconds)
   if (Random.nextBoolean()) {
     dataDepositsState.value = LceState.Content
     depositsData.value = depositList
   } else {
-    dataState.value = LceState.Error("Failed to load data")
-  }
-  if (dataAccountsState.value != LceState.Content && dataDepositsState.value != LceState.Content) {
-    dataState.value = LceState.Error("Failed to load data")
-  } else {
-    dataState.value = LceState.Content
+    dataDepositsState.value = LceState.Error("Failed to load data")
   }
 }
-
-val dataState = MutableStateFlow<LceState>(LceState.None)
