5d9033cf4ad0546e931e7e84ff4a5225671c105a
Danya Goga <CryingSakura69@gmail.com>
2024-04-23 14:43:43 +0200

    in ViewState delete GeneralState(loadingState), in Screen now displayed through when(){}

--- products/ui/src/main/java/ru/kode/base/internship/ui/home/ProductsHomeScreen.kt
+++ products/ui/src/main/java/ru/kode/base/internship/ui/home/ProductsHomeScreen.kt
@@ -15,10 +15,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
@@ -29,7 +28,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -41,25 +39,25 @@ import ru.kode.base.internship.core.domain.entity.LceState
 import ru.kode.base.internship.products.ui.R
 import ru.kode.base.internship.ui.component.AccountItem
 import ru.kode.base.internship.ui.component.DepositItem
+import ru.kode.base.internship.ui.component.ShimmerComponent
+import ru.kode.base.internship.ui.core.uikit.component.PrimaryButton
 import ru.kode.base.internship.ui.core.uikit.screen.AppScreen
 import ru.kode.base.internship.ui.core.uikit.theme.AppTheme
-import ru.kode.base.internship.ui.error.ProductsHomeErrorScreen
+import ru.kode.base.internship.ui.error.ProductsHomeError
 import ru.kode.base.internship.ui.error.SomethingWrongComponent
-import ru.kode.base.internship.ui.shimmer.ShimmerScreen
 import ru.kode.base.internship.ui.shimmer.shimmerLoadingAnimation
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ProductsHomeScreen(viewModel: ProductsHomeViewModel = daggerViewModel()) = AppScreen(
-  viewModel = viewModel, intents = rememberViewIntents()
+  viewModel = viewModel,
+  intents = rememberViewIntents()
 ) { state, intents ->
   OnBackPressedHandler(onBack = intents.navigateOnBack)
 
-
   val pullRefreshState = rememberPullRefreshState(
-    refreshing = state.loadingLceState is LceState.Loading,
+    refreshing = false,
     onRefresh = intents.refreshData
-
   )
 
   Box(
@@ -70,41 +68,22 @@ fun ProductsHomeScreen(viewModel: ProductsHomeViewModel = daggerViewModel()) = A
       .pullRefresh(pullRefreshState)
       .background(AppTheme.colors.backgroundPrimary),
   ) {
-    if (state.loadingLceState is LceState.Loading) {
+    if (state.loadingAccountsLceStates is LceState.Loading && state.loadingDepositsLceStates is LceState.Loading) {
       Column {
-        Row(
-          modifier = Modifier
-            .fillMaxWidth()
-            .height(64.dp)
-            .padding(start = 16.dp),
-          verticalAlignment = Alignment.CenterVertically
-        ) {
-          Row(
-            modifier = Modifier
-              .size(width = 160.dp, height = 32.dp)
-              .clip(shape = RoundedCornerShape(16.dp))
-          )
-          {
-            Box(
-              modifier = Modifier
-                .shimmerLoadingAnimation()
-                .fillMaxSize()
-            )
-          }
-        }
-        ShimmerScreen(
+        TopHomeShimmerItem()
+        ShimmerComponent(
           modifier = Modifier
             .fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(16.dp))
-        ShimmerScreen(
+        ShimmerComponent(
           modifier = Modifier
             .fillMaxWidth()
         )
       }
     }
 
-    if (state.loadingLceState is LceState.Content) {
+    if (state.loadingAccountsLceStates is LceState.Content || state.loadingDepositsLceStates is LceState.Content) {
       LazyColumn(modifier = Modifier.fillMaxSize()) {
         item {
           Text(
@@ -116,139 +95,161 @@ fun ProductsHomeScreen(viewModel: ProductsHomeViewModel = daggerViewModel()) = A
             textAlign = TextAlign.Center
           )
         }
-        //Счета
         item {
-          Row(
+          Text(
             modifier = Modifier
               .fillMaxWidth()
               .background(AppTheme.colors.backgroundSecondary)
-          ) {
-            Text(
-              modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, top = 16.dp),
-              text = stringResource(id = R.string.accounts),
-              textAlign = TextAlign.Start
-            )
+              .padding(start = 16.dp, end = 16.dp, top = 16.dp),
+            text = stringResource(id = R.string.accounts),
+            textAlign = TextAlign.Start
+          )
+        }
+        when (state.loadingAccountsLceStates) {
+          LceState.Content -> {
+            if (state.accounts.isNotEmpty()) {
+              fun LazyListScope.accounts(accounts: List<Account>) = items(accounts) { account ->
+                AccountItem(
+                  modifier = Modifier.background(AppTheme.colors.backgroundSecondary),
+                  onAccountItemClick = intents.accountDetailsRequested,
+                  cards = account.cards,
+                  onCardClick = intents.cardDetailsRequested,
+                  money = Money(balance = account.balance, currency = account.currency),
+                  unLastAccountInList = account != state.accounts.last(),
+                )
+              }
+              accounts(state.accounts)
+            }
           }
 
-        }
-        //Список счетов
-        if (state.loadingAccountsLceStates is LceState.Content) {
-          if (state.accounts.isNotEmpty()) {
-            items(state.accounts) { account ->
-              AccountItem(
-                modifier = Modifier.background(AppTheme.colors.backgroundSecondary),
-                onAccountItemClick = intents.checkAccountDetails,
-                cardsList = account.cards,
-                onCardClick = intents.checkCardDetails,
-                accountBalance = account.balance,
-                unLastAccountInList = account != state.accounts.last(),
-                accountCurrency = account.currency
+          is LceState.Error -> {
+            item {
+              SomethingWrongComponent(onClick = intents.refreshAccountData)
+            }
+          }
+
+          LceState.Loading -> {
+            item {
+              ShimmerComponent(
+                modifier = Modifier
+                  .fillMaxWidth()
               )
             }
           }
-        } else {
-          item {
-            ShimmerScreen(
-              modifier = Modifier
-                .fillMaxWidth()
-            )
-            SomethingWrongComponent(onClick = intents.refreshData)
-          }
+
+          LceState.None -> {}
         }
         item {
           Spacer(modifier = Modifier.height(16.dp))
         }
-
-        //Вклады
         item {
-          Row(
+          Text(
             modifier = Modifier
               .fillMaxWidth()
               .background(AppTheme.colors.backgroundSecondary)
-          ) {
-            Text(
-              modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, top = 16.dp),
-              text = stringResource(id = R.string.сontribution),
-              textAlign = TextAlign.Start
-            )
-          }
+              .padding(16.dp),
+            text = stringResource(id = R.string.deposits),
+            textAlign = TextAlign.Start
+          )
         }
-        //Список вкладов
-        if (state.loadingDepositsLceStates is LceState.Content) {
-          if (state.deposits.isNotEmpty()) {
-            items(state.deposits) { depAcc ->
-              Column(Modifier.background(color = AppTheme.colors.backgroundSecondary)){
-                DepositItem(
-                  modifier = Modifier.background(AppTheme.colors.backgroundSecondary),
-                  onDepositClick = intents.checkDepositDetails,
-                  depositBalance = depAcc.balance,
-                  depositCloseDate = depAcc.closeDate,
-                  depositCurrency = depAcc.currency,
-                  depositName = depAcc.name,
-                  depositRate = depAcc.rate,
-                )
-                if (depAcc != state.deposits.last()) {
-                  Divider(
-                    modifier = Modifier.padding(start = 72.dp, end = 16.dp),
-                    color = AppTheme.colors.contendSecondary,
-                    thickness = 2.dp
+        when (state.loadingDepositsLceStates) {
+          LceState.Content -> {
+            if (state.deposits.isNotEmpty()) {
+              fun LazyListScope.deposits(deposits: List<Deposit>) = items(deposits) { depAcc ->
+                Column(Modifier.background(color = AppTheme.colors.backgroundSecondary)) {
+                  DepositItem(
+                    modifier = Modifier.background(AppTheme.colors.backgroundSecondary),
+                    onDepositClick = intents.depositDetailsRequested,
+                    depositCloseDate = depAcc.closeDate,
+                    depositCurrency = depAcc.currency,
+                    depositName = depAcc.name,
+                    depositRate = depAcc.rate,
+                    money = Money(balance = depAcc.balance, currency = depAcc.currency)
                   )
+                  if (depAcc != state.deposits.last()) {
+                    Divider(
+                      modifier = Modifier.padding(start = 72.dp, end = 16.dp),
+                      color = AppTheme.colors.contendSecondary,
+                      thickness = 2.dp
+                    )
+                  }
                 }
               }
-
+              deposits(state.deposits)
             }
           }
-        } else {
-          item {
-            ShimmerScreen(
-              modifier = Modifier
-                .fillMaxWidth()
-            )
-            SomethingWrongComponent(onClick = intents.refreshData)
+
+          is LceState.Error -> {
+            item {
+              SomethingWrongComponent(onClick = intents.refreshDepositData)
+            }
           }
+
+          LceState.Loading -> {
+            item {
+              ShimmerComponent(
+                modifier = Modifier
+                  .fillMaxWidth()
+              )
+            }
+          }
+
+          LceState.None -> {}
         }
-        //Button
         if (state.loadingAccountsLceStates is LceState.Content && state.loadingDepositsLceStates is LceState.Content) {
           item {
-            Button(
+            PrimaryButton(
               modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
                 .heightIn(min = 52.dp),
-              onClick = { intents.openNewAccountOrProduct },
-              colors = ButtonDefaults.buttonColors(
-                backgroundColor = AppTheme.colors.primaryButton,
-                contentColor = Color.White
-              ),
-              content = {
-                Text(
-                  text = stringResource(id = R.string.open_new_account_or_product),
-                  fontSize = 15.sp
-                )
-              },
-              shape = RoundedCornerShape(32.dp),
+              onClick = intents.openNewAccountOrProduct,
+              text = stringResource(id = R.string.open_new_account_or_product)
             )
           }
         }
       }
     }
-    if (state.loadingLceState == LceState.Error("Failed to load data")) {
-      ProductsHomeErrorScreen(
+    if (state.loadingAccountsLceStates == LceState.Error(
+        "Failed to load data"
+      ) && state.loadingDepositsLceStates == LceState.Error(
+        "Failed to load data"
+      )
+    ) {
+      ProductsHomeError(
         onRefreshClick = intents.refreshData,
         onCloseClick = intents.navigateOnBack,
-        refreshing = state.loadingLceState == LceState.Loading
       )
     }
     PullRefreshIndicator(
       modifier = Modifier.align(Alignment.TopCenter),
-      refreshing = state.loadingLceState is LceState.Loading,
+      refreshing = false,
       state = pullRefreshState,
       backgroundColor = AppTheme.colors.contendAccentPrimary,
       contentColor = AppTheme.colors.contendAccentTertiary
     )
+  }
+}
+
+@Composable
+fun TopHomeShimmerItem() {
+  Row(
+    modifier = Modifier
+      .fillMaxWidth()
+      .height(64.dp)
+      .padding(start = 16.dp),
+    verticalAlignment = Alignment.CenterVertically
+  ) {
+    Row(
+      modifier = Modifier
+        .size(width = 160.dp, height = 32.dp)
+        .clip(shape = RoundedCornerShape(16.dp))
+    ) {
+      Box(
+        modifier = Modifier
+          .shimmerLoadingAnimation()
+          .fillMaxSize()
+      )
+    }
   }
 }
