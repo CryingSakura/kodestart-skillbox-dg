927367e99fd524c0db5a670389b3bb8ee777a7e3
Danya Goga <CryingSakura69@gmail.com>
2024-04-26 03:42:48 +0200

    Change classes for deposits & accounts Lists

--- products/ui/src/main/java/ru/kode/base/internship/ui/home/ProductsHomeViewState.kt
+++ products/ui/src/main/java/ru/kode/base/internship/ui/home/ProductsHomeViewState.kt
@@ -2,11 +2,14 @@ package ru.kode.base.internship.ui.home
 
 import androidx.compose.runtime.Immutable
 import ru.kode.base.internship.core.domain.entity.LceState
+import ru.kode.base.internship.products.domain.entity.AccountDataEntity
+import ru.kode.base.internship.products.domain.entity.CardDataEntity
+import ru.kode.base.internship.products.domain.entity.DepositDataEntity
 
 @Immutable
 data class ProductsHomeViewState(
   val loadingAccountsLceStates: LceState = LceState.None,
   val loadingDepositsLceStates: LceState = LceState.None,
-  val deposits: List<Deposit> = emptyList(),
-  val accounts: List<Account> = emptyList(),
+  val deposits: List<DepositDataEntity> = emptyList(),
+  val accounts: List<AccountDataEntity> = emptyList(),
 )
