a6499c1df543c793f49d38f386975cad458de81d
Danya Goga <CryingSakura69@gmail.com>
2024-04-26 03:28:10 +0200

    Deposit Repository + Impl

--- products/data/src/main/java/ru/kode/base/internship/products/data/repository/DepositRepositoryImpl.kt
+++ products/data/src/main/java/ru/kode/base/internship/products/data/repository/DepositRepositoryImpl.kt
@@ -2,7 +2,9 @@ package ru.kode.base.internship.products.data.repository
 
 import com.squareup.anvil.annotations.ContributesBinding
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.update
 import ru.kode.base.core.di.AppScope
 import ru.kode.base.internship.products.data.MockGetData
 import ru.kode.base.internship.products.domain.entity.DepositDataEntity
@@ -10,14 +12,16 @@ import ru.kode.base.internship.products.domain.repository.DepositRepository
 import javax.inject.Inject
 
 @ContributesBinding(AppScope::class)
-class DepositRepositoryImpl @Inject constructor(): DepositRepository {
-  override val depositsFlow: Flow<List<DepositDataEntity>>
-    get() = flow { MockGetData.getDeposits()  }
+class DepositRepositoryImpl @Inject constructor() : DepositRepository {
+  override val depositsFlow = MutableStateFlow(MockGetData.getDeposits())
+
+  override fun fetchDeposits() {
+    depositsFlow.update { MockGetData.getDeposits() }
+  }
 
   override fun term(id: DepositDataEntity.Id): Flow<DepositDataEntity> {
     return flow {
       MockGetData.getDeposits().find { it.depositId == id }
     }
   }
-
 }
--- products/domain/src/main/java/ru/kode/base/internship/products/domain/repository/DepositRepository.kt
+++ products/domain/src/main/java/ru/kode/base/internship/products/domain/repository/DepositRepository.kt
@@ -1,9 +1,12 @@
 package ru.kode.base.internship.products.domain.repository
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import ru.kode.base.internship.products.domain.entity.DepositDataEntity
 
 interface DepositRepository {
-  val depositsFlow: Flow<List<DepositDataEntity>>
+  val depositsFlow: MutableStateFlow<List<DepositDataEntity>>
   fun term(id: DepositDataEntity.Id): Flow<DepositDataEntity>
+  fun fetchDeposits() {
+  }
 }
